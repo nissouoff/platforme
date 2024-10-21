7e636023632124b45c7d05e6fb53b873ab372695
nissouoff <156860993+nissouoff@users.noreply.github.com>
2024-10-21 21:14:38 +0100

    Delete V 1.0 directory

--- V 1.0/login.js
+++
@@ -1,6 +0,0 @@
-
-    document.querySelectorAll('.contenu-a div').forEach((element) => {
-        element.addEventListener('click', function() {
-            this.classList.toggle('active'); // Activer ou désactiver la classe active au tap
-        });
-    });
