acc0b54a7fc91870d16e69af659901e4ae4e505b
nissouoff <156860993+nissouoff@users.noreply.github.com>
2024-10-22 02:26:26 +0100

    Add files via upload

---
+++ login.js
@@ -0,0 +1,5 @@
+document.querySelectorAll('.contenu-a div').forEach((element) => {
+    element.addEventListener('click', function() {
+        this.classList.toggle('active'); // Activer ou désactiver la classe active au tap
+    });
+});
