ecdfc68afaf46aa61e80b28fe29731d534efa536
nissouoff <156860993+nissouoff@users.noreply.github.com>
2024-10-24 20:00:43 +0100

    Add files via upload

--- V 1.0/js/login1.js
+++ V 1.0/js/login1.js
@@ -1,8 +1,8 @@
-onload = function() {
+/*onload = function() {
     // Desactiver le clic droit
-   /* document.addEventListener('contextmenu', function(e) {
+    document.addEventListener('contextmenu', function(e) {
         e.preventDefault()
-    })*/
+    })
 
     // Désactiver la touche F12
     document.addEventListener('keydown', function(e) {
@@ -14,4 +14,31 @@ onload = function() {
     // Fermer automatiquement l'inspecteur d'éléments
 }
 
+function isMobileDevice() {
+    return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+}
 
+function detectDevTools() {
+    if (!isMobileDevice()) {
+        const threshold = 160;
+        const widthThreshold = window.outerWidth - window.innerWidth > threshold;
+        const heightThreshold = window.outerHeight - window.innerHeight > threshold;
+        
+        if (widthThreshold || heightThreshold || window.devtools.isOpen) {
+            console.log("Les DevTools sont ouvertes !");
+            window.location.href = "https://www.google.com";
+        }
+    }
+}
+
+if (!isMobileDevice()) {
+    setInterval(detectDevTools, 1000);
+
+    window.addEventListener('devtoolschange', event => {
+        if(event.detail.isOpen) {
+            window.location.href = "https://www.google.com";
+        }
+    });
+}
+
+*/
