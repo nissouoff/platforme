3f631db007fd3984eacef5c0194fe490bc3fdf3b
nissouoff <156860993+nissouoff@users.noreply.github.com>
2024-11-03 01:10:18 +0100

    Update server.js

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(express.static('public'));
 
 // Route pour rediriger vers index.html
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
+    res.sendFile(__dirname + '/V 1.0/index.html');
 });
 
 // Middleware
